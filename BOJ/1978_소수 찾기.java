d0329fa458e6f7e237940fed6d6f318daa467969
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2019-07-18 21:53:45 +0900

    BOJ

--- BOJ/1978_소수 찾기.java
+++ BOJ/1978_소수 찾기.java
@@ -1,3 +1,16 @@
+/*
+[문제]
+주어진 수 N개 중에서 소수가 몇 개인지 찾아서 출력하는 프로그램을 작성하시오.
+
+[입력]
+첫 줄에 수의 개수 N이 주어진다. N은 100이하이다. 다음으로 N개의 수가 주어지는데 수는 1,000 이하의 자연수이다.
+
+[출력]
+주어진 수들 중 소수의 개수를 출력한다.
+
+[풀이]
+1~1000 까지의 수 중에서 소수를 판별하고 소수를 ArrayList에 오름차순으로 넣는다. Collections.binarySearch로 찾으려는 수가 소수이면 cnt를 증가시키고 cnt를 출력한다.
+*/
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
