3f88cd152ec6c28f76a41971ad72205a118f8d54
yoonseop <sys09270883@gmail.com>
2019-07-19 17:43:09 +0900

    BOJ

---
+++ BOJ/10827_a^b.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigDecimal;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		BigDecimal res = new BigDecimal(st.nextToken()).pow(Integer.parseInt(st.nextToken()));
+		
+		bw.write(res.toPlainString());
+		
+		bw.close();
+		br.close();
+	}
+}
