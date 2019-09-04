93c7eb9ef0152d57425eeeab4422b087ebb21242
yoonseop <sys09270883@gmail.com>
2019-09-04 18:53:23 +0900

    BOJ

---
+++ BOJ/5014_스타트링크.java
@@ -0,0 +1,95 @@
+/*
+https://www.acmicpc.net/problem/5014
+[문제]
+강호는 코딩 교육을 하는 스타트업 스타트링크에 지원했다. 오늘은 강호의 면접날이다. 하지만, 늦잠을 잔 강호는 스타트링크가 있는 건물에 늦게 도착하고 말았다.
+
+스타트링크는 총 F층으로 이루어진 고층 건물에 사무실이 있고, 스타트링크가 있는 곳의 위치는 G층이다. 
+강호가 지금 있는 곳은 S층이고, 이제 엘리베이터를 타고 G층으로 이동하려고 한다.
+
+보통 엘리베이터에는 어떤 층으로 이동할 수 있는 버튼이 있지만, 강호가 탄 엘리베이터는 버튼이 2개밖에 없다. 
+U버튼은 위로 U층을 가는 버튼, D버튼은 아래로 D층을 가는 버튼이다. (만약, U층 위, 또는 D층 아래에 해당하는 층이 없을 때는, 엘리베이터는 움직이지 않는다)
+
+강호가 G층에 도착하려면, 버튼을 적어도 몇 번 눌러야 하는지 구하는 프로그램을 작성하시오. 
+만약, 엘리베이터를 이용해서 G층에 갈 수 없다면, "use the stairs"를 출력한다.
+
+[입력]
+첫째 줄에 F, S, G, U, D가 주어진다. (1 ≤ S, G ≤ F ≤ 1000000, 0 ≤ U, D ≤ 1000000) 건물은 1층부터 시작하고, 가장 높은 층은 F층이다.
+
+[출력]
+첫째 줄에 강호가 S층에서 G층으로 가기 위해 눌러야 하는 버튼의 수의 최솟값을 출력한다. 만약, 엘리베이터로 이동할 수 없을 때는 "use the stairs"를 출력한다.
+
+[풀이]
+1층 보다 내려갈 수 없고 F층 보다 올라갈 수 없는 것을 유의한다.
+U만큼 올라가고 D만큼 내려가게 BFS한다.
+
+*/
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int F, S, G, U, D;
+	static boolean[] visited;
+	static int[] df = new int[2];
+	
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        F = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        G = Integer.parseInt(st.nextToken());
+        U = Integer.parseInt(st.nextToken());
+        D = Integer.parseInt(st.nextToken());
+        visited = new boolean[F + 1];
+        df[0] = U;
+        df[1] = -D;
+        int res = BFS();
+        
+        bw.write(res != -1 ? String.valueOf(res) : "use the stairs");
+        bw.close();
+        br.close();
+    }
+    
+    private static int BFS() {
+    	Queue<Node> queue = new LinkedList<Node>();
+    	queue.add(new Node(S, 0));
+    	visited[S] = true;
+    	
+    	while (!queue.isEmpty()) {
+    		Node cur = queue.poll();
+    		int curFloor = cur.floor;
+    		int curCnt = cur.cnt;
+    		
+    		if (curFloor == G)
+    			return curCnt;
+    		
+    		for (int i = 0; i < 2; i++) {
+				int nextFloor = curFloor + df[i];
+				
+				if (nextFloor < 1 || nextFloor > F)
+					continue;
+				
+				if (visited[nextFloor])
+					continue;
+				
+				visited[nextFloor] = true;
+				queue.add(new Node(nextFloor, curCnt + 1));
+			}
+    	}
+    	
+    	return -1;
+    }
+    
+}
+
+class Node {
+	int floor, cnt;
+	
+	public Node(int floor, int cnt) {
+		this.floor = floor;
+		this.cnt = cnt;
+	}
+}
