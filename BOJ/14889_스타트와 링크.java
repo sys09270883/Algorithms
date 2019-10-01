f70b44d8af08b87862c832ce0c0d3d58f1546095
yoonseop <sys09270883@gmail.com>
2019-10-01 09:36:31 +0900

    BOJ

---
+++ BOJ/14889_스타트와 링크.java
@@ -0,0 +1,97 @@
+/*
+https://www.acmicpc.net/problem/14889
+[문제]
+오늘은 스타트링크에 다니는 사람들이 모여서 축구를 해보려고 한다. 축구는 평일 오후에 하고 의무 참석도 아니다. 
+축구를 하기 위해 모인 사람은 총 N명이고 신기하게도 N은 짝수이다. 이제 N/2명으로 이루어진 스타트 팀과 링크 팀으로 사람들을 나눠야 한다.
+
+BOJ를 운영하는 회사 답게 사람에게 번호를 1부터 N까지로 배정했고, 아래와 같은 능력치를 조사했다. 
+능력치 Sij는 i번 사람과 j번 사람이 같은 팀에 속했을 때, 팀에 더해지는 능력치이다. 팀의 능력치는 팀에 속한 모든 쌍의 능력치 Sij의 합이다.
+ Sij는 Sji와 다를 수도 있으며, i번 사람과 j번 사람이 같은 팀에 속했을 때, 팀에 더해지는 능력치는 Sij와 Sji이다.
+
+N=4이고, S가 아래와 같은 경우를 살펴보자.
+
+i\j	1	2	3	4
+1	 	1	2	3
+2	4	 	5	6
+3	7	1	 	2
+4	3	4	5	 
+예를 들어, 1, 2번이 스타트 팀, 3, 4번이 링크 팀에 속한 경우에 두 팀의 능력치는 아래와 같다.
+
+스타트 팀: S12 + S21 = 1 + 4 = 5
+링크 팀: S34 + S43 = 2 + 5 = 7
+1, 3번이 스타트 팀, 2, 4번이 링크 팀에 속하면, 두 팀의 능력치는 아래와 같다.
+
+스타트 팀: S13 + S31 = 2 + 7 = 9
+링크 팀: S24 + S42 = 6 + 4 = 10
+축구를 재미있게 하기 위해서 스타트 팀의 능력치와 링크 팀의 능력치의 차이를 최소로 하려고 한다. 
+위의 예제와 같은 경우에는 1, 4번이 스타트 팀, 2, 3번 팀이 링크 팀에 속하면 스타트 팀의 능력치는 6, 링크 팀의 능력치는 6이 되어서 차이가 0이 되고 이 값이 최소이다.
+
+[입력]
+첫째 줄에 N(4 ≤ N ≤ 20, N은 짝수)이 주어진다. 둘째 줄부터 N개의 줄에 S가 주어진다. 
+각 줄은 N개의 수로 이루어져 있고, i번 줄의 j번째 수는 Sij 이다. Sii는 항상 0이고, 나머지 Sij는 1보다 크거나 같고, 100보다 작거나 같은 정수이다.
+
+[출력]
+첫째 줄에 스타트 팀과 링크 팀의 능력치의 차이의 최솟값을 출력한다.
+
+[풀이]
+1. 팀을 나눈다. (DFS, 백트래킹)
+2. 기저사례에서 스타트팀과 링크팀의 차이를 구하고 값을 갱신한다.
+
+*/
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int N, res;
+	static int[][] arr;
+	static boolean[] visited;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N + 1][N + 1];
+		visited = new boolean[N + 1];
+		res = Integer.MAX_VALUE;
+		StringTokenizer st = null;
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= N; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		DFS(0, 1);
+		
+		bw.write(String.valueOf(res));
+		bw.close();
+		br.close();
+	}
+	
+	private static void DFS(int depth, int n) {
+		if (depth == N / 2) {
+			int startSum = 0, linkSum = 0;
+			for (int i = 1; i <= N; i++) {
+				for (int j = 1; j <= N; j++) {
+					if (visited[i] && visited[j])
+						startSum += arr[i][j];
+					else if (!visited[i] && !visited[j])
+						linkSum += arr[i][j];
+				}
+			}
+			
+			int gap = Math.abs(startSum - linkSum);
+			res = res > gap ? gap : res;
+			return;
+		}
+		
+		for (int i = n; i < N; i++) {
+			visited[i] = true;
+			DFS(depth + 1, i + 1);
+			visited[i] = false;
+		}
+	}
+	
+}
