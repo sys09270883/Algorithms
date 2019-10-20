666dae5e7e473b7393511b585be43ffbcbefe2b8
yoonseop <sys09270883@gmail.com>
2019-10-20 23:06:01 +0900

    quick sort - middle pivot

--- Sort/Sort/QuickSort.cpp
+++ Sort/Sort/QuickSort.cpp
@@ -4,36 +4,26 @@ const int len = 12;
 
 using namespace std;
 
-int partition(int * arr, int s, int e) {
-	int l = s, r = e + 1;
-	int pivot = arr[l];
-
-	do
-	{
-		do
-		{
-			l++;
-		} while (l <= e && arr[l] < pivot);
-
-		do
-		{
-			r--;
-		} while (r >= s && arr[r] > pivot);
-
-		if (l < r)
-			swap(arr, l, r);
-	} while (l < r);
-
-	swap(arr, s, r);
-
-	return r;
-}
-
 void quick_sort(int * arr, int s, int e) {
 	if (s < e) {
-		int p = partition(arr, s, e);
-		quick_sort(arr, s, p - 1);
-		quick_sort(arr, p + 1, e);
+		int pivot = arr[(s + e) / 2];
+		int l = s, r = e;
+
+		while (l <= r) {
+			while (arr[l] < pivot)
+				l++;
+
+			while (arr[r] > pivot)
+				r--;
+
+			if (l <= r) {
+				swap(arr, l, r);
+				l++; r--;
+			}
+		}
+
+		quick_sort(arr, s, r);
+		quick_sort(arr, l, e);
 	}
 }
 
