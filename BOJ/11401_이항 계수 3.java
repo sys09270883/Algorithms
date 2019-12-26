b0724606cd9c9bc686ded3e329aa7905590c128c
yoonseop <sys09270883@gmail.com>
2019-12-27 01:06:19 +0900

    페르마 소정리, 분할정복, 이항계수

---
+++ BOJ/11401_이항 계수 3.java
@@ -0,0 +1,117 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static FastIO io = new FastIO();
+
+    public static void main(String... args) throws IOException {
+        int N = io.nextInt(), K = io.nextInt();
+        long MOD = (long)1e9 + 7;
+        long[] factorial = new long[N + 1];
+        factorial[0] = 1;
+        factorial[1] = 1;
+        for (int i = 2; i < N + 1; i++) {
+            factorial[i] = (factorial[i - 1] * i) % MOD;
+        }
+        long d = (factorial[K] * factorial[N - K]) % MOD;
+        long idx = MOD - 2;
+        long fermat = 1;
+
+        while (idx > 0) {
+            if ((idx & 1) == 1) {
+                fermat *= d;
+                fermat %= MOD;
+            }
+            d = (d * d) % MOD;
+            idx >>= 1;
+        }
+        long res = ((factorial[N] % MOD) * (fermat % MOD)) % MOD;
+        io.write(res);
+    }
+
+}
+
+class FastIO {
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static StringTokenizer st;
+
+    FastIO() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+
+    String next() {
+        while (st == null || !st.hasMoreTokens()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return st.nextToken();
+    }
+
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+        String str = null;
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return str;
+    }
+
+    void write(double d) throws IOException {
+        bw.write(String.valueOf(d));
+        close();
+    }
+
+    void write(int i) throws IOException {
+        bw.write(String.valueOf(i));
+        close();
+    }
+
+    void write(long l) throws IOException {
+        bw.write(String.valueOf(l));
+        close();
+    }
+
+    void write(StringBuilder sb) throws IOException {
+        bw.write(sb.toString().trim());
+        close();
+    }
+
+    void write(String str) throws IOException {
+        bw.write(str.trim());
+        close();
+    }
+
+    void write(char[] str) throws IOException {
+        int len = str.length;
+        for (int i = 0; i < len; i++) {
+            bw.write(str[i]);
+        }
+        close();
+    }
+
+    void close() throws IOException {
+        bw.close();
+        br.close();
+    }
+}
