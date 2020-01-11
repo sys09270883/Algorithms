b8557ac7c8dbc0f6ac5855441ca2206bd4fa26e9
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2020-01-11 12:41:36 +0900

    유클리드 호제법

---
+++ BOJ/2609_최대공약수와 최소공배수.java
@@ -0,0 +1,105 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static FastIO io = new FastIO();
+
+    public static void main(String... args) throws IOException {
+        int a = io.nextInt(), b = io.nextInt();
+        StringBuilder res = new StringBuilder();
+        int gcd = GCD(a, b);
+        int lcm = a * b / gcd;
+        res.append(gcd).append(' ').append(lcm);
+        io.write(res);
+    }
+
+    private static int GCD(int a, int b) {
+        if (b == 0)
+            return a;
+        else
+            return GCD(b, a % b);
+    }
+
+}
+
+class FastIO {
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static StringTokenizer st;
+
+    FastIO() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+
+    String next() {
+        while (st == null || !st.hasMoreTokens()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return st.nextToken();
+    }
+
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+        String str = null;
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return str;
+    }
+
+    void write(double d) throws IOException {
+        bw.write(String.valueOf(d));
+        close();
+    }
+
+    void write(char c) throws IOException {
+        bw.write(c);
+        close();
+    }
+
+    void write(int i) throws IOException {
+        bw.write(String.valueOf(i));
+        close();
+    }
+
+    void write(long l) throws IOException {
+        bw.write(String.valueOf(l));
+        close();
+    }
+
+    void write(StringBuilder sb) throws IOException {
+        bw.write(sb.toString().trim());
+        close();
+    }
+
+    void write(String str) throws IOException {
+        bw.write(str.trim());
+        close();
+    }
+
+    void close() throws IOException {
+        bw.close();
+        br.close();
+    }
+}
