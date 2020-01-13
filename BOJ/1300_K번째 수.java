d0d8dbd0c31cdfa1331a4196af863faf10b434f1
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2020-01-13 14:26:01 +0900

    이분탐색

---
+++ BOJ/1300_K번째 수.java
@@ -0,0 +1,107 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static FastIO io = new FastIO();
+
+    public static void main(String... args) throws IOException {
+        int N = io.nextInt(), k = io.nextInt(), low = 1, high = k, res = -1;
+        while (low <= high) {
+            int m = (low + high) / 2, cnt = 0;
+            for (int i = 1; i < N + 1; i++) {
+                cnt += Math.min(m / i, N);
+            }
+            if (cnt < k)
+                low = m + 1;
+            else {
+                high = m - 1;
+                res = m;
+            }
+        }
+
+        io.write(res);
+    }
+
+}
+
+class FastIO {
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static StringTokenizer st;
+
+    FastIO() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+
+    String next() {
+        while (st == null || !st.hasMoreTokens()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return st.nextToken();
+    }
+
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+        String str = null;
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return str;
+    }
+
+    void write(double d) throws IOException {
+        bw.write(String.valueOf(d));
+        close();
+    }
+
+    void write(char c) throws IOException {
+        bw.write(c);
+        close();
+    }
+
+    void write(int i) throws IOException {
+        bw.write(String.valueOf(i));
+        close();
+    }
+
+    void write(long l) throws IOException {
+        bw.write(String.valueOf(l));
+        close();
+    }
+
+    void write(StringBuilder sb) throws IOException {
+        bw.write(sb.toString().trim());
+        close();
+    }
+
+    void write(String str) throws IOException {
+        bw.write(str.trim());
+        close();
+    }
+
+    void close() throws IOException {
+        bw.close();
+        br.close();
+    }
+}
