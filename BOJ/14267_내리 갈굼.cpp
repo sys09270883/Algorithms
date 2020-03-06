6f3c089c47286da128a9f78973214681060faba8
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2020-03-07 00:14:46 +0900

    세그먼트 트리, Lazy Propagation, 오일러 투어 테크닉

---
+++ BOJ/14267_내리 갈굼.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+#define endl '\n'
+#define pb push_back
+#define pii pair<int, int>
+
+int N, M, H, o;
+vector<int> l, r, tree, lazy;
+vector<vector<int>> adj;
+
+void DFS(int cur) {
+    l[cur] = ++o;
+    for (auto& next : adj[cur]) {
+        DFS(next);
+    }
+    r[cur] = o;
+}
+
+void updateLazy(int n, int s, int e) {
+    if (lazy[n]) {
+        if (s ^ e) {
+            lazy[n << 1] += lazy[n];
+            lazy[n << 1 | 1] += lazy[n];
+        }
+        tree[n] += (e - s + 1) * lazy[n];
+        lazy[n] = 0;
+    }
+}
+
+int update(int l, int r, int d, int n, int s, int e) {
+    updateLazy(n, s, e);
+    if (l > e || r < s)
+        return tree[n];
+    if (l <= s && e <= r) {
+        if (s ^ e) {
+            lazy[n << 1] += d;
+            lazy[n << 1 | 1] += d;
+        }
+        return tree[n] += (e - s + 1) * d;
+    }
+    int m = s + e >> 1;
+    return tree[n] = update(l, r, d, n << 1, s, m) + update(l, r, d, n << 1 | 1, m + 1, e);
+}
+
+int query(int i, int n, int s, int e) {
+    updateLazy(n, s, e);
+    if (i > e || i < s)
+        return 0;
+    if (s == e)
+        return tree[n];
+    int m = s + e >> 1;
+    return tree[n] = query(i, n << 1, s, m) + query(i , n << 1 | 1, m + 1, e);
+}
+
+int main() {
+    FASTIO
+    cin >> N >> M;
+    l.resize(N + 1);
+    r.resize(N + 1);
+    H = 1 << (int)ceil(log2(N)) + 1;
+    tree.resize(H);
+    lazy.resize(H);
+    adj.resize(N + 1, vector<int>());
+    int x;
+    cin >> x;
+    for (int i = 2; i < N + 1; i++) {
+        cin >> x;
+        adj[x].pb(i);
+    }
+    DFS(1);
+    for (int i = 0; i < M; i++) {
+        int a, b;
+        cin >> a >> b;
+        update(l[a], r[a], b, 1, 1, N);
+    }
+    for (int i = 1; i < N + 1; i++) {
+        cout << query(l[i], 1, 1, N) << ' ';
+    }
+}
