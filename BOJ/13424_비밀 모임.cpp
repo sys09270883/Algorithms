bac467580ddca38305dc995f4a9ca230f9e987f9
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2020-04-13 13:27:20 +0900

    다익스트라

--- BOJ/13424_비밀 모임.cpp
+++ BOJ/13424_비밀 모임.cpp
@@ -15,75 +15,6 @@ struct Node {
     }
 };
 
-const int INF = 987654321;
-int N, M, K, minv, mini;
-
-void dijkstra(int idx, int n, vector<vector<Node>>& adj, vector<vector<int>>& dists) {
-    priority_queue<Node> pq;
-    pq.push({n, dists[idx][n] = 0});
-    while (!pq.empty()) {
-        int cur = pq.top().idx;
-        int cd = pq.top().dist;
-        pq.pop();
-        if (cd > dists[idx][cur])
-            continue;
-        for (auto& n : adj[cur]) {
-            int next = n.idx;
-            int nd = n.dist;
-            if (dists[idx][next] > dists[idx][cur] + nd)
-                pq.push({next, dists[idx][next] = dists[idx][cur] + nd});
-        }
-    }
-}
-
-int main() {
-    FASTIO
-    int T;
-    cin >> T;
-    while (T--) {
-        minv = INF, mini = -1;
-        cin >> N >> M;
-        vector<vector<Node>> adj(N + 1);
-        for (int i = 1; i < N + 1; i++) {
-            vector<Node> v;
-            adj.pb(v);
-        }
-        for (int i = 0; i < M; i++) {
-            int a, b, c;
-            cin >> a >> b >> c;
-            adj[a].pb({b, c});
-            adj[b].pb({a, c});
-        }
-        cin >> K;
-        vector<vector<int>> dists;
-        for (int i = 0; i < K; i++) {
-            vector<int> v;
-            v.resize(N + 1, INF);
-            dists.pb(v);
-        }
-        for (int i = 0; i < K; i++) {
-            int n;
-            cin >> n;
-            dijkstra(i, n, adj, dists);
-        }
-        for (int i = 1; i < N + 1; i++) {
-            int sum = 0;#include <bits/stdc++.h>
-using namespace std;
-#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-typedef long long ll;
-#define endl '\n'
-#define pb push_back
-#define pii pair<int, int>
-#define pll pair<ll, ll>
-
-struct Node {
-    int idx, dist;
-    Node(int idx, int dist) : idx(idx), dist(dist) {}
-    bool operator<(const Node& n) const {
-        return this->dist < n.dist;
-    }
-};
-
 vector<vector<Node>> adj;
 vector<vector<int>> dists;
 const int INF = 987654321;
@@ -132,17 +63,6 @@ int main() {
         }
         for (int i = 1; i < N + 1; i++) {
             int sum = 0;
-            for (int j = 0; j < K; j++) {
-                sum += dists[j][i];
-            }
-            if (minv > sum) {
-                minv = sum;
-                mini = i;
-            }
-        }
-        cout << mini << endl;
-    }
-}
             for (int j = 0; j < K; j++) {
                 sum += dists[j][i];
             }
