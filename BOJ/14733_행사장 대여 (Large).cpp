cebe0313cc3b32129eb1333a25b915ad42a21388
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2020-06-17 13:08:16 +0900

    plane sweeping

--- BOJ/14733_행사장 대여 (Large).cpp
+++ BOJ/14733_행사장 대여 (Large).cpp
@@ -26,34 +26,30 @@ struct Point {
 };
 
 const int MAX = 1e5, HALF = 5e4;
-int N, px, H;
+int N, x, H;
 ll res;
 vector<Point> v;
-vector<int> tree, cnt;
+vector<int> tree, lazy;
 
-void update(int l, int r, int d, int n, int s, int e) {
+int get(int n, int s, int e) {
+    return lazy[n] ? e - s + 1 : tree[n];
+}
+
+int update(int l, int r, int d, int n, int s, int e) {
     if (l > e || r < s)
-        return;
-    if (l <= s && e <= r)
-        cnt[n] += d;
-    else {
-        int m = s + e >> 1;
-        update(l, r, d, n << 1, s, m);
-        update(l, r, d, n << 1 | 1, m + 1, e);
+        return get(n, s, e);
+    if (l <= s && e <= r) {
+        lazy[n] += d;
+        return get(n, s, e);
     }
-    if (cnt[n])
-        tree[n] = e - s + 1;
-    else {
-        if (s ^ e)
-            tree[n] = tree[n << 1] + tree[n << 1 | 1];
-        else
-            tree[n] = 0;
-    }
+    int m = s + e >> 1;
+    tree[n] = update(l, r, d, n << 1, s, m) + update(l, r, d, n << 1 | 1, m + 1, e);
+    return get(n, s, e);
 }
 
 void resize() {
     H = 1 << (int)ceil(log2(MAX)) + 1;
-    tree.resize(H), cnt.resize(H);
+    tree.resize(H), lazy.resize(H);
 }
 
 int main() {
@@ -63,15 +59,15 @@ int main() {
     for (int i = 0; i < N; i++) {
         int x1, y1, x2, y2;
         cin >> x1 >> y1 >> x2 >> y2;
-        v.pb({x1 + HALF, y1 + HALF, y2 + HALF, 1});
-        v.pb({x2 + HALF, y1 + HALF, y2 + HALF, -1});
+        v.pb({x1, y1 + HALF, y2 + HALF - 1, 1});
+        v.pb({x2, y1 + HALF, y2 + HALF - 1, -1});
     }
     sort(all(v));
-    px = v[0].x;
+    x = v[0].x;
     for (auto& i : v) {
-        res += (i.x - px) * tree[1];
-        update(i.y1, i.y2 - 1, i.end, 1, 0, MAX);
-        px = i.x;
+        res += 1ll * (i.x - x) * tree[1];
+        update(i.y1, i.y2, i.end, 1, 0, MAX);
+        x = i.x;
     }
     cout << res;
 }
