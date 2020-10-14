70b5228cbb936460315562b6436c5f6e01f20783
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2020-10-14 21:11:13 +0900

    수학

---
+++ BOJ/19950_3차원 막대기 연결하기.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pll = pair<ll, ll>;
+using pli = pair<ll, int>;
+using pic = pair<int, char>;
+using pci = pair<char, int>;
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#define FASTIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define endl '\n'
+#define pb push_back
+#define ft first
+#define sd second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define cpr(x) sort(all(x)), (x).erase(unique(all(x)), (x).end());
+
+int main()
+{
+    FASTIO
+    int x1, y1, z1, x2, y2, z2;
+    cin >> x1 >> y1 >> z1 >> x2 >> y2 >> z2;
+    int d = round(sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2)));
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (auto &i : v)
+    {
+        cin >> i;
+    }
+    v.pb(d);
+    sort(all(v));
+    int sum = 0;
+    for (int i = 0; i < v.size() - 1; i++)
+    {
+        sum += v[i];
+    }
+    if (v.back() <= sum)
+        cout << "YES";
+    else
+        cout << "NO";
+}
