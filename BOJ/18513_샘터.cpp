8ab7c51e9366afe753bc7cae5761472f8a51fd15
Yoonseop Shin <sys09270883@gmail.com>
2021-01-26 21:47:56 +0900

    BFS

--- BOJ/18513_샘터.cpp
+++ BOJ/18513_샘터.cpp
@@ -1,54 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-typedef long long ll;
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pll = pair<ll, ll>;
+using pli = pair<ll, int>;
+using pic = pair<int, char>;
+using pci = pair<char, int>;
+using pss = pair<string, string>;
+using tiii = tuple<int, int, int>;
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(NULL);
 #define endl '\n'
 #define pb push_back
-#define pii pair<int, int>
+#define ft first
+#define sd second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define cpr(x) sort(all(x)), (x).erase(unique(all(x)), (x).end());
 
-struct Node {
-    int idx, dir, diff;
-    Node(int idx, int dir, int diff) : idx(idx), dir(dir), diff(diff) {}
-};
-
-const int MAX = (int)1e8;
+const int MAX = 1e8;
 int N, K;
 vector<int> s;
-unordered_map<int, int> vis;
+map<int, int> vis;
 
-ll BFS() {
+ll BFS()
+{
     ll ret = 0, cnt = 0;
-    queue<Node> q;
-    for (auto& i : s) {
+    queue<tiii> q;
+    for (auto &i : s)
+    {
         vis[i] = 0;
         q.push({i, -1, 0});
         q.push({i, 1, 0});
     }
-    while (!q.empty() && cnt < K) {
-        int cur = q.front().idx;
-        int dir = q.front().dir;
-        int diff = q.front().diff + 1;
+    while (!q.empty() && cnt < K)
+    {
+        auto [cur, dir, diff] = q.front();
         q.pop();
         int next = cur + dir;
-        if (next < -MAX || next > MAX)
-            continue;
-        if (vis.find(next) != vis.end()) {
+        diff++;
+        if (vis.find(next) == vis.end())
+        {
+            vis[next] = diff;
+            cnt++;
+            q.push({next, dir, diff});
+        }
+        else
+        {
             if (vis[next] <= diff)
                 continue;
+            vis[next] = diff;
+            cnt++;
+            q.push({next, dir, diff});
         }
-        vis[next] == diff;
-        cnt++;
-        q.push({next, dir, diff});
         ret += diff;
     }
     return ret;
 }
 
-int main() {
+int main()
+{
     FASTIO
     cin >> N >> K;
     s.resize(N);
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++)
+    {
         cin >> s[i];
     }
     cout << BFS();
